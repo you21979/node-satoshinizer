6676b41e3080c3bfa705b2afd0e956853564c0c4
you2 <you2197901@gmail.com>
2015-06-23 13:48:59 +0900

    mod

--- example/ex1.js
+++ example/ex1.js
@@ -33,12 +33,14 @@ var satoshinizer = require('..');
 var satoshis = xs.map(function(x){return satoshinizer.fromNumber(x)})
 var w = satoshis.map(function(x){return satoshinizer.toNumber(x)})
 var s = satoshis.map(function(x){return satoshinizer.toHuman(x)})
-var s2 = satoshis.map(function(x){return satoshinizer.toHuman(x,0)})
+var s2 = satoshis.map(function(x){return satoshinizer.toHuman(x,0,satoshinizer.ROUND_DOWN)})
+var s3 = satoshis.map(function(x){return satoshinizer.toHuman(x,0,satoshinizer.ROUND_UP)})
 
 console.log(satoshis)
 console.log(w)
 console.log(s)
 console.log(s2)
+console.log(s3)
 
 
 
